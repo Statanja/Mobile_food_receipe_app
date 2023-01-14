d82b0658b798e0eaccf0fb28801713fbbe86d907
Statanja <statanja175@gmail.com>
2023-01-14 12:49:04 +0530

    Developing

---
+++ main.js
@@ -0,0 +1,95 @@
+
+const meals = document.getElementById('meals');
+
+getRandomMeal();
+fetchFavMeals();
+
+ async function getRandomMeal(){
+     const resp =  await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
+
+     const respData = await resp.json();
+     const randomMeal = respData.meals[0]
+
+     console.log(randomMeal)
+     addMealToFav(randomMeal,true)
+ }
+
+ async function getMealById(id){
+    const resp = await fetch("https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + id);
+
+    const respData = await resp.json();
+    const meal = respData.meals[0];
+    return meal;
+ }
+
+ async function getMealsBySearch(term){
+    const meals = await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=" + term);
+ }
+
+
+
+    function addMealsToLs(mealId){
+           const mealIds = getMealsFromLs()
+
+            localStorage.setItem('mealId',JSON.stringify([...mealIds,mealId]))
+    }
+    function removeMealsFromLs(mealId){
+        const mealIds = getMealsFromLs()
+        localStorage.setItem('mealId',JSON.stringify(mealIds.filter(id => id !== mealId)));
+    }
+
+    function getMealsFromLs(){
+        const mealIds = JSON.parse(localStorage.getItem('mealIds'));
+        return mealIds === null ? [] : mealIds;
+    }
+
+
+    async function fetchFavMeals(){
+        const mealIds = getMealsFromLs();
+
+        const meals = [];
+
+        for (let i=0; i<mealIds.length; i++){
+            const mealId = mealIds[i];
+            meal =  await getMealById(mealId);
+            meals.push(meal);
+            addMealToFav(meal)
+        }
+        console.log(meals)
+    }
+
+function addMealToFav(mealData ,random= false){
+
+    const meal = document.createElement("div");
+    meal.classList.add('meal');
+    meal.innerHTML = `
+        <div class="meal-header">
+            ${random ? `
+            <span class="random"> Random Recipe </span>`
+        : ""}
+            <img
+                src="${mealData.strMealThumb}"
+                alt="${mealData.strMeal}"
+            />
+        </div>
+        <div class="meal-body">
+            <h4>${mealData.strMeal}</h4>
+            <button class="fav-btn">
+                <i class="fas fa-heart"></i>
+            </button>
+        </div>
+    `;
+    const btn = meal.querySelector('.meal-body>.fav-btn');
+
+    btn.addEventListener('click',(e)=>{
+        if(btn.classList.contains("active")){
+            removeMealsFromLs(mealData.idMeal);
+            btn.classList.remove('active')
+        } else {
+            addMealsToLs(mealData.idMeal);
+            btn.classList.add("active")
+        }
+    })
+
+    meals.appendChild(meal)
+}
